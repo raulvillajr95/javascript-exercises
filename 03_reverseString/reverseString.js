a4f7301a8f3782ad0e019ca8385b12f83cf48a1f
Raul <raulvillajr95@gmail.com>
2022-05-23 21:14:36 -0700

    Pass 4 tests for exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let list = []
 
+  for (let i = str.length-1; i >= 0; i--) {
+    list.push(str[i])
+  }
+
+  return list.join('')
 };
 
 // Do not edit below this line
