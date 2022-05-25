a583e408da1596e0c83377d4dc7749db32ca4861
Raul <raulvillajr95@gmail.com>
2022-05-24 22:20:10 -0700

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let count = 0;
 
+  if (num1 < 0 || num2 < 0 || typeof num1 != 'number' || typeof num2 != 'number') {
+    return 'ERROR'
+  }
+
+  if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
+      count += i
+    }
+  } else if (num1 > num2) {
+    for (let i = num2; i <= num1; i++) {
+      count += i
+    }
+  }
+
+  return count
 };
 
 // Do not edit below this line
