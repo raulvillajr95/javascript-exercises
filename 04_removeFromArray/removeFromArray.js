5803251263ec9a2d07bceb7c29c23ae17eeb4c8a
Raul <raulvillajr95@gmail.com>
2022-05-24 19:16:32 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+  for (let i = 1; i < arguments.length; i++) {
+    if (arr.indexOf(arguments[i]) == -1) {
+      continue
+    } else {
+      arr.splice(arr.indexOf(arguments[i]), 1)
+    }
+  }
+  return arr
 };
 
 // Do not edit below this line
