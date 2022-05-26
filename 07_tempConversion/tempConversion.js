d9f70f295fe3ef591dcfeb7d0cc8aaa0e23f62df
Raul <raulvillajr95@gmail.com>
2022-05-25 17:53:00 -0700

    Fully pass exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(deg) {
+  let fullDecimal = (deg - 32) * (5/9)
+  return parseFloat(fullDecimal.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  let fullDecimal = (deg * (9/5)) + 32
+  return parseFloat(fullDecimal.toFixed(1))
 };
 
 // Do not edit below this line
