c8e6a6d2f314d8625eec5c850cbd4a25f1fd7a1b
Raul <raulvillajr95@gmail.com>
2022-06-19 21:41:47 -0700

    Complete Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  if (arr.length == 0) {
+    return 0
+  } else if (arr.length == 1) {
+    return arr[0]
+  }
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for (let i = 0; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) {
+    return 1;
+  }
+  let total = 1;
+  for (let i = 2; i <= a; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
